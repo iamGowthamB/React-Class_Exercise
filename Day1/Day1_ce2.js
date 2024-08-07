17c5c2eca49fd381d0e785ee04b4b2e3f5a42ff0
iamGowthamB <150770038+iamGowthamB@users.noreply.github.com>
2024-08-07 06:54:05 +0530

    Add files via upload

---
+++ Day1/Day1_ce2.js
@@ -0,0 +1,26 @@
+import React from 'react';
+function obj()
+{
+    let myobject={name:"Madras"};
+    let newobject=myobject;
+    myobject.name="chennai";
+    alert("Check Console Output");
+    console.log(myobject);
+    console.log(newobject);
+    
+
+    let myarray=["a","e","i","o"];
+    let newarray=myarray;
+    myarray.push("u");
+    for(var i=0;i<myarray.length;i++)
+    {
+        console.log(myarray[i]);
+    }
+}
+function ce2 (){
+    return(
+        <div className='ce2'>
+            <button onClick={obj}>CE2</button>
+        </div>
+    )
+}

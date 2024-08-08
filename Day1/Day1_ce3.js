820a6dfebcf14d61a4ba3df06082faba5e2c3887
iamGowthamB <150770038+iamGowthamB@users.noreply.github.com>
2024-08-08 19:03:02 +0530

    Add files via upload

---
+++ Day1/Day1_ce3.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function World()
+{
+    return(
+        <div>
+            <h1>This is World Functional Component</h1>
+        </div>
+    )
+}
+export default World;
